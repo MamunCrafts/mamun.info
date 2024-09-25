331603afc5c90bd83c5a76894eb025b41fcaf593
MamunCrafts <mamun@talentpro.global>
2024-09-25 21:59:19 +0600

    sidebar and project showcasing modified

--- src/components/Research.tsx
+++ src/components/Research.tsx
@@ -1,12 +1,22 @@
+import { useEffect, useRef } from "react";
+
 export default function Research() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 4.0; // Set playback speed to 2x
+    }
+  }, []);
   return (
+    
     <>
       <div className="bg-slate-200 bg-opacity-30 backdrop-blur-sm shadow-lg w-full md:w-[56%] mx-auto p-[2%]">
         <div className="ml-2">
           <p className="text-black italic text-3xl md:text-5xl text-center">
             <span className="border-black border-b-[3px]">Academic Projects</span>
           </p>
-          <p className="text-black pt-[1%] text-xl md:text-2xl  "> <span className="bg-yellow-300">Student Information Management System</span></p>
+          <p className="text-black pt-[3%] text-xl md:text-2xl  "> <span className="bg-yellow-300">Student Information Management System</span></p>
           <p className="text-black pt-[1%] text-lg md:text-xl  "> <span className="bg-white shadow-sm ">Supervisor : DR. MD. EMDADUL HAQUE</span></p>
           <p className="text-black pt-[1%] text-lg md:text-xl"> <span className="bg-white shadow-sm text-blue-600 underline cursor-pointer">Email : haque-ice@ru.ac.bd</span></p>
           <p className="text-black pt-[1%] text-lg md:text-xl "> <span className="bg-white shadow-sm">Professor,Department of Information and Communication Engineering</span></p>
@@ -37,34 +47,43 @@ export default function Research() {
             <span className="border-black border-b-[3px]"> Projects</span>
           </p>
        
-          <h1 className="text-2xl md:text-3xl text-black font-bold  py-[3%]"><span className="bg-yellow-300">Fanfare Bangladesh Ltd.</span></h1>
-      <p className="text-lg text-black text-justify mb-4">
+          <h1 className="text-2xl md:text-3xl text-black font-bold  py-[4%] "><span className="bg-yellow-300">Fanfare Bangladesh Ltd.</span></h1>
+      <p className="text-lg text-slate-700 text-justify mb-4">
         Fanfare Bangladesh Ltd. is a pioneering Social Commerce platform, established in 2019 as a joint venture between Singapore and Bangladesh. It offers a vibrant space for users to share and explore a limitless collection of short videos while facilitating innovative digital marketing solutions for brands.
       </p>
       <h2 className="text-2xl text-black font-semibold mb-4">Key Features:</h2>
-      <ul className="list-disc text-black ml-5 space-y-3">
+      <ul className="list-disc text-black ml-5 space-y-3 text-slate-700">
         <li>
-          <span className="font-semibold">Shoppable User-Generated Content:</span> Fanfare empowers brands to transform any user-generated content into shoppable videos, seamlessly connecting them with target customers and driving impactful marketing results.
+          <span className="font-semibold  ">Shoppable User-Generated Content:</span> Fanfare empowers brands to transform any user-generated content into shoppable videos, seamlessly connecting them with target customers and driving impactful marketing results.
         </li>
         <li>
-          <span className="font-semibold">AI-Driven Marketing:</span> Leveraging artificial intelligence and advanced analytics, the platform optimizes advertising spend across thousands of creator channels in diverse sectors such as fashion, beauty, technology, video games, cooking, automotive, travel, and luxury.
+          <span className="font-semibold ">AI-Driven Marketing:</span> Leveraging artificial intelligence and advanced analytics, the platform optimizes advertising spend across thousands of creator channels in diverse sectors such as fashion, beauty, technology, video games, cooking, automotive, travel, and luxury.
         </li>
         <li>
-          <span className="font-semibold">E-Commerce Enablement:</span> Verified brands, vendors, and SMEs can register to showcase and sell products directly on the platform, offering a streamlined e-commerce experience with dynamic features designed to enhance business growth.
+          <span className="font-semibold ">E-Commerce Enablement:</span> Verified brands, vendors, and SMEs can register to showcase and sell products directly on the platform, offering a streamlined e-commerce experience with dynamic features designed to enhance business growth.
         </li>
         <li>
-          <span className="font-semibold">Socio-Commerce Ecosystem:</span> Fanfare transcends traditional social media and marketing solutions by creating a comprehensive socio-commerce ecosystem. It serves as a one-stop solution for individuals and businesses, bridging the gap between content creation, consumer engagement, and commerce.
+          <span className="font-semibold ">Socio-Commerce Ecosystem:</span> Fanfare transcends traditional social media and marketing solutions by creating a comprehensive socio-commerce ecosystem. It serves as a one-stop solution for individuals and businesses, bridging the gap between content creation, consumer engagement, and commerce.
         </li>
       </ul>
-      <p className="text-lg text-black text-justify mt-4">
+      <p className="text-lg text-slate-700 text-justify mt-4">
         With its unique blend of social interaction and commercial functionality, Fanfare aims to foster an environment where users and businesses alike can thrive, making it a platform that truly caters to everyone.
       </p>
-          <div className="flex items-center justify-center ">
-  <video width="800" autoPlay muted loop className="rounded-lg shadow-lg">
-    <source src="./fanfare.mp4" type="video/mp4" />
-    Your network is slow, please wait for the video to load.
-  </video>
- 
+          <div className="flex flex-col pt-[2%] items-center justify-center ">
+          <video
+      ref={videoRef}
+      width="800"
+      autoPlay
+      muted
+      loop
+      className="rounded-lg shadow-lg"
+    >
+      <source src="./fanfare.mp4" type="video/mp4" />
+      Your network is slow, please wait for the video to load.
+    </video>
+    <p className="text-lg text-black text-justify mt-4">
+    Showcasing the Key Features of the Fanfare Project
+      </p>
 </div>
         </div>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,13 @@ export default function Home() {
     academics: false,
     contests: false,
   });
+
+  const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  // Close the navbar if clicked outside
+  useOnClickOutside(ref, () => setIsOpen(false));
+
   const handleTabChange = (tab: string) => {
     setState({
       experience: false,
@@ -27,55 +34,70 @@ export default function Home() {
       contests: false,
       [tab]: true,
     });
-    // setIsOpen(false);
+    setIsOpen(false); 
   };
-  const [isOpen, setIsOpen] = useState(false);
-  const ref = useRef<HTMLDivElement>(null);
-  useOnClickOutside(ref, () => setIsOpen(false));
-  return ( <>
-   <Head>
+
+  return (
+    <>
+      <Head>
         <title>Mamun | Info</title>
         <meta name="description" content="This is a description of the page content." />
         <meta property="og:title" content="Japan Professor" />
       </Head>
-  <div
-      className="bg-gray-200  bg-[url('https://s3.ap-south-1.amazonaws.com/bucket.portfolio.mamun/bgv3.png')] 
-     bg-fixed bg-cover bg-center"
-    >
 
-      <div className="relative inline-block  pt-[2%] pl-[2%] md:hidden">
+      <div className="bg-gray-200 bg-[url('https://s3.ap-south-1.amazonaws.com/bucket.portfolio.mamun/bgv3.png')] bg-fixed bg-cover bg-center">
+        {/* Mobile Navbar */}
+        <div className="md:hidden relative pt-[2%] pl-[2%]">
+          <img
+            onClick={() => setIsOpen(!isOpen)}
+            src="./menu.png"
+            alt="menu"
+            className="w-[40px] h-[40px] hover:scale-125 hover:cursor-pointer"
+          />
 
-        <img
-          onClick={() => setIsOpen(!isOpen)}
-          src="./menu.png"
-          alt="menu"
-          className="w-[40px] h-[40px] hover:scale-110 hover:cursor-pointer"
-        />
-
-        {/* Dropdown menu */}
-        {isOpen && (
-          <div  ref={ref} className=" absolute right-0 w-10/12 mt-2   rounded-md  z-20">
-            <div className="py-1 items-start " >
-              <button
-                type="button"
-                onClick={() => handleTabChange('experience')}
-                className={`nav-button ${state.experience ? 'focus' : ''}`}
-              >              Experience
-              </button>
-              <button type="button" onClick={() => handleTabChange("research")} className={`nav-button ${state.research ? 'focus' : ''}`}>
-                Research
-              </button>
-
-              <button type="button" onClick={() => handleTabChange("aboutMe")} className={`nav-button ${state.aboutMe ? 'focus' : ''}`}>About Me</button>
-
-              <button type="button" onClick={() => handleTabChange("academics")} className={`nav-button ${state.academics ? 'focus' : ''}`}>Academics</button>
-              <button type="button" onClick={() => handleTabChange("contests")} className={`nav-button ${state.contests ? 'focus' : ''}`}>Contests</button>
-
+          {/* Dropdown Menu for Mobile */}
+          {isOpen && (
+            <div ref={ref} className="absolute top-14 left-0 w-64 bg-white rounded-md shadow-lg z-20">
+              <nav className="flex  flex-col space-y-2 py-4 px-6">
+                <button
+                  type="button"
+                  onClick={() => handleTabChange('experience')}
+                  className={`nav-button ${state?.experience&&"focus"}`}
+                >
+                  Experience
+                </button>
+                <button
+                  type="button"
+                  onClick={() => handleTabChange('research')}
+                  className={`nav-button ${state?.research&&"focus"}`}
+                >
+                  Research
+                </button>
+                <button
+                  type="button"
+                  onClick={() => handleTabChange('aboutMe')}
+                  className={`nav-button ${state?.aboutMe&&"focus"}`}
+                >
+                  About Me
+                </button>
+                <button
+                  type="button"
+                  onClick={() => handleTabChange('academics')}
+                  className={`nav-button ${state?.academics&&"focus"}`}
+                >
+                  Academics
+                </button>
+                <button
+                  type="button"
+                  onClick={() => handleTabChange('contests')}
+                  className={`nav-button ${state?.contests&&"focus"}`}
+                >
+                  Contests
+                </button>
+              </nav>
             </div>
-          </div>
-        )}
-      </div>
-
+          )}
+        </div>
       <Profile />
 
       <Navbar handleTabChange={handleTabChange} />
